87851fa8ed0c38a0f671630797ed345202a0f7ae
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-02 17:39:35 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import express from "express";
+
+import "./database";
+// @types/express
+const app = express();
+
+app.get("/test", (request, response) => {
+  // Request => Entrando
+  // Response => Saindo
+  return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW método POST");
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
