c221931e041bdd041a677c53c702342fe5a8938a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-04 15:17:35 -0300

    Ajustando validação de token

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,12 +1,15 @@
 import { Request, Response, NextFunction, request } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(
+export async function ensureAdmin(
   resquest: Request,
   response: Response,
   next: NextFunction
 ) {
   const { user_id } = request;
-  const admin = true;
+  const usersRepositories = getCustomRepository(UsersRepositories);
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return next();
