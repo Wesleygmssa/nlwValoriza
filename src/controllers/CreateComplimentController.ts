725a0f627a623210a2ac2974081b2ac84b22537a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-07-04 08:40:50 -0300

    Criado rotas de elegios(compliments)

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,7 +3,17 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 class CreateComplimentContoller {
   async handle(request: Request, response: Response) {
+    const { tag_id, user_sender, user_receiver, message } = request.body;
     const createComplimentService = new CreateComplimentService();
+
+    const compliment = await createComplimentService.execute({
+      tag_id,
+      user_sender,
+      user_receiver,
+      message,
+    });
+
+    return response.json(compliment);
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -4,17 +4,19 @@ import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserSerivice } from "./services/AuthenticateUserService";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentContoller } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserControler = new AuthenticateUserController();
+const createComlimentController = new CreateComplimentContoller();
 
 // router.use(ensureAdmin);
 router.post("/users", createUserController.handle);
-
 router.post("/tags", ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserControler.handle);
+router.post("/compliments", createComlimentController.handle);
 
 export { router };
